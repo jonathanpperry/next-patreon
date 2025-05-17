bc00b5db51d2154c82238df94eb15c6dd540cf6f
Jon Perry <jpper@umich.edu>
2025-05-17 00:13:49 -0400

    Initialize shadcn

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      {/* Left side */}
+      <div>
+        <Link href="/">
+          <h2>Creator Site</h2>
+        </Link>
+      </div>
+
+      {/* Right side */}
+      <div>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+
+        <SignedOut>
+          <SignInButton mode="modal" />
+        </SignedOut>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
