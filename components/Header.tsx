b5b94f0b99535d7a246a6123c1ad08931ae5929d
Jon Perry <jpper@umich.edu>
2025-05-20 21:23:13 -0400

    Build schema types, pull from sanity studio, and finish up header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,54 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
+import { HeartIcon } from "lucide-react";
+import { getSiteSettings } from "@/sanity/lib/siteSettings/getSiteSettings";
+import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
 
-function Header() {
+async function Header() {
+  const siteSettings = await getSiteSettings();
+  console.log("site settings: ", siteSettings);
+
   return (
-    <header>
+    <header className="flex items-center justify-between p-4 border-b border-gray-200">
       {/* Left side */}
       <div>
         <Link href="/">
-          <h2>Creator Site</h2>
+          {siteSettings?.headerLogo ? (
+            <Image
+              src={urlFor(siteSettings?.headerLogo).url()}
+              alt={siteSettings?.headerLogo?.alt || "Logo"}
+              width={100}
+              height={100}
+              className="w-10 h-10 object-cover rounded-full aspect-square"
+            />
+          ) : (
+            <h2>{siteSettings?.siteTitle}</h2>
+          )}
         </Link>
       </div>
 
       {/* Right side */}
-      <div>
+      <div className="flex items-center gap-4">
         <SignedIn>
-          <UserButton />
+          <div className="flex items-center gap-2 hover:gap-4 px-2 py-2 hover:px-4 hover:bg-gray-100 transition-all duration-200 border border-gray-200 rounded-full">
+            <UserButton />
+          </div>
         </SignedIn>
 
         <SignedOut>
-          <SignInButton mode="modal" />
+          <Button
+            asChild
+            variant="outline"
+            className="px-4 py-2 bg-red-500 text-white rounded-lg"
+          >
+            <div>
+              <SignInButton mode="modal" />
+              <HeartIcon className="w-4 h-4" />
+            </div>
+          </Button>
         </SignedOut>
       </div>
     </header>
---
+++ sanity/schemaTypes/siteSettings.ts
@@ -0,0 +1,110 @@
+import { SettingsIcon } from "lucide-react";
+import { defineArrayMember, defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "siteSettings",
+  title: "Site Settings",
+  type: "document",
+  description: "Settings for your site",
+  icon: SettingsIcon,
+  preview: {
+    select: {
+      title: "siteTitle",
+    },
+    prepare({ title }) {
+      return {
+        title,
+        media: SettingsIcon,
+      };
+    },
+  },
+  fields: [
+    {
+      name: "siteTitle",
+      title: "Site Title",
+      type: "string",
+      description: "The name of your creator page",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "description",
+      title: "Site Description",
+      type: "text",
+      description: "A brief description of your creator page and what you do",
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "headerLogo",
+      title: "Header Logo",
+      type: "image",
+      description: "Logo displayed in the header of your site",
+      //   options: {
+      //     hotspot: true,
+      //   },
+      fields: [
+        {
+          name: "alt",
+          title: "Alternative Text",
+          type: "string",
+          description: "Important for SEO and accessibility",
+        },
+      ],
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "mainHeroImage",
+      title: "Main Hero Image",
+      type: "image",
+      description: "The main banner image displayed on your home page",
+      fields: [
+        {
+          name: "alt",
+          title: "Alternative Text",
+          type: "string",
+          description: "Important for SEO and accessibility",
+        },
+      ],
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: "logo",
+      title: "Logo",
+      type: "image",
+      description: "The logo of your creator page",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          title: "Alternative Text",
+          type: "string",
+          description: "Important for SEO and accessibility",
+        },
+      ],
+      validation: (Rule) => Rule.required(),
+    },
+    defineField({
+      name: "socialMediaLinks",
+      title: "Social Media Links",
+      type: "array",
+      description: "Links to your social media profiles",
+      of: [
+        defineArrayMember({
+          type: "object",
+          name: "socialLink",
+          fields: [
+            { name: "platform", title: "Platform", type: "string" },
+            { name: "url", title: "URL", type: "url" },
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: "callToActionText",
+      title: "Call To Action Text",
+      type: "text",
+      rows: 2,
+    }),
+  ],
+});
